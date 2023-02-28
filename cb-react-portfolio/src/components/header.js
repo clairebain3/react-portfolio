c390b3d36675ceb746cd10d69ac00345e2d66dea
Claire Bain <claire.bain3@gmail.com>
2023-02-28 14:52:35 -0800

    sets up basic template for portfolio

---
+++ cb-react-portfolio/src/components/header.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Header(){
+    return(<div>
+        <nav>
+        <p>Claire Bain</p>
+        <p>About Me</p>
+        <p>Portfolio</p>
+        <p>Contact</p>
+        <p>Resume</p>
+        </nav>
+    </div>);
+
+}
+export default Header;
