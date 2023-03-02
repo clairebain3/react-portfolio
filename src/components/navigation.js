10a52b24c602699e69605c8a068b08d428bd8acf
Claire Bain <claire.bain3@gmail.com>
2023-03-02 15:57:53 -0800

    adds css for navbar and array mapping for portfolio

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,24 +2,35 @@ import React, { useState } from 'react';
 import AboutMe from './aboutme';
 import Footer from './footer';
 import Resume from './resume';
+import Portfolio from './portfolio';
 // import all the files? this will be the page generated?
 
-export default function Navigation(props){
+export default function Navigation(){
     const [input, setInout] = useState('');
     let [section, setSection] = useState(AboutMe);
 
-    const sections = [AboutMe, 'Portfolio', 'Contact', Resume]
+    const sections = [AboutMe, Portfolio, 'Contact', Resume]
 
     return(
         <div>
             <header>
-            <nav>
-        <p>Claire Bain</p>
-        <p onClick={() => setSection(sections[0])}>About Me</p>
-        <p onClick={() => setSection(sections[1])}>Portfolio</p>
-        <p onClick={() => setSection(sections[2])}>Contact</p>
-        <p onClick={() => setSection(sections[3])}>Resume</p>
-        </nav>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Claire Bain</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a onClick={() => setSection(sections[0])} class="nav-link active" aria-current="page" href="#">About Me</a>
+        <a onClick={() => setSection(sections[1])}class="nav-link" href="#">Portfolio</a>
+        <a onClick={() => setSection(sections[2])}class="nav-link" href="#">Contact</a>
+        <a onClick={() => setSection(sections[3])}class="nav-link" href ="#">Resume</a>
+      </div>
+    </div>
+  </div>
+</nav>
+     
 
             </header>
             <p>{section}</p>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,1 +1,26 @@
 // my project data can be an object that i pass in as props and iterate over with the component
+import React  from 'react';
+import Projects from './projectdata.json';
+const myvar = Projects[0].name
+const props = Projects
+const test = Projects.map(displayProjects)
+console.log(test)
+function displayProjects(item){
+    return(item.name)
+
+}
+function Portfolio(props){
+    return(<div>
+        {Projects.map(displayProjects)}
+
+        {/* <p>About Me</p>
+        <p>Portfolio</p>
+        <p>Contact</p>
+        <p>Resume</p> */}
+        {/* </nav> */}
+    </div>);
+
+
+
+}
+export default Portfolio;
