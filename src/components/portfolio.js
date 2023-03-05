8981a1c0b957c0ed85ecf11cd8d2086aea533131
Claire Bain <claire.bain3@gmail.com>
2023-03-05 15:36:17 -0800

    sets up portfolio page

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,16 +1,38 @@
 // my project data can be an object that i pass in as props and iterate over with the component
 import React  from 'react';
-import Projects from './projectdata.json';
+import Projects from './/../assets/projectdata.json';
+import pic from './/../assets/BOBCongratsScreenshot.jpeg';
+import pic1 from './/../assets/techblogpic.png';
+import pic2 from './/../assets/Weather-pic.png';
+import pic3 from './/../assets/gif-movie-site.png';
+import pic4 from './/../assets/pwatexteditorpic.png';
+import pic5 from './/../assets/Schedule-pic.png';
+
+
+
+const url = [pic, pic1, pic2, pic3, pic4, pic5]
+
 const myvar = Projects[0].name
 const props = Projects
 const test = Projects.map(displayProjects)
 console.log(test)
 function displayProjects(item){
-    return(item.name)
+    
+
+    return(<div class = "picContainer">
+    <img class="portfoliopic" src = {url[item.pic]}/>
+    <div  >
+    <p class = "picContent">{item.name}</p>
+    <a class = "picContent" href = {item.application}>app</a>
+    <a class = "picContent" href = {item.repo}>github</a>
+    </div>
+    </div>
+    )
+        // item.name)
 
 }
 function Portfolio(props){
-    return(<div>
+    return(<div class = "projcontainer">
         {Projects.map(displayProjects)}
 
         {/* <p>About Me</p>
